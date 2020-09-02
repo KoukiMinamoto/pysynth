9267acdc137062e07e01315464b83846d9a2be8b
Kouki <tomatomusicstudio@gmail.com>
2020-09-02 15:44:39 +0900

    aaa

--- example_sampler_loop.py
+++ example_sampler_loop.py
@@ -21,24 +21,24 @@ synth = Series(pitch=440, rate=44100, bufsize=512)
 # In[3]:
 
 
-synth.stack(MIDIkeyboard())
+synth.stack(MidiFromPCkey())
 #synth.stack(MonoMeledy(72, 3.0))
 
 
-# In[ ]:
+# In[4]:
 
 
-#sam = synth.stack(Sampler(wavefile="wizavo_outfile/men/wa-_edited.wav"))
-osc = synth.stack(ToufuWave(interval=12))
-#sam.loopON(pos=0.5, length=0.1, fade=0.9)
+sam = synth.stack(Sampler(wavefile="wizavo_outfile/men/wa-_edited.wav"))
+#osc = synth.stack(ToufuWave(interval=12))
+sam.loopON(pos=0.5, length=0.1, fade=0.6)
 #amp = synth.stack(WavWriter(volume=0.1, file_name="result/melo_ha-.wav"))
 amp = synth.stack(SimpleAmp(volume=0.5))
 
 
-# In[ ]:
+# In[5]:
 
 
-env = Envelope(A=0.01, D=0.03, S=0.5, R=0.2)
+env = Envelope(A=1.0, D=0.03, S=0.5, R=1.0)
 #env3 = Envelope(A=0.05, D=0.3, S=0.0, R=0.2)
 #env3.assign(osc.fine, scale=100.)
 #env2 = ArduinoController(com='/dev/cu.usbmodem14301', baudrate=9600)
